061c00599627694b6ed37ff4842cb44afd459e80
Hope <hope.roeser@gmail.com>
2018-06-16 10:12:16 -0500

    first commit

---
+++ src/minesweeper.js
@@ -0,0 +1,11 @@
+const blankLine = ' | | ';
+console.log("This is what a blank board would look like.");
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+const guessLine = '1| | ';
+const bombLine = ' |B| ';
+console.log("This is what a board with a bomb and a guess would look like:");
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
