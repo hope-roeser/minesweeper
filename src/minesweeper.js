bfc74df829eefd128cbc2329583f0b7d45e5a2b9
Hope <hope.roeser@gmail.com>
2018-07-03 18:49:13 -0500

    minesweeper pt 3

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,20 +1,41 @@
-const printBoard = board => {
-    console.log('Current Board:')
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = [];
+        for (i = 0; i < numberOfRows; i++) {
+            let row = [];
+            for (x=0; x < numberOfColumns; x++) {
+                row.push(' ');
+            }
+            board.push(row);
+        }
+        return board;
 }
-    
 
-const board = [
-    [' ',' ',' '],
-    [' ',' ',' '],
-    [' ',' ',' ']
-];
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+     let board = [];
+        for (i = 0; i < numberOfRows; i++) {
+            let row = [];
+            for (x=0; x < numberOfColumns; x++) {
+                row.push(null);
+            }
+            board.push(row);
+        }
+    let numberOfBombsPlaced = 0
+    while (numberOfBombsPlaced < numberOfBombs) {
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        board[randomRowIndex][randomColumnIndex] = 'B'
+        numberOfBombsPlaced++
+        //currently, a bomb may be placed on top of an existing bomb. this will be fixed in a later version
+    }
+        return board;
+}
 
-printBoard(board);
+const printBoard = board => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+}
 
-board[0][1] = '1';
-printBoard(board);
-board[2][2] = 'B';
-printBoard(board);
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ' + printBoard(playerBoard));
+console.log('Bomb Board: ' + printBoard(bombBoard));
